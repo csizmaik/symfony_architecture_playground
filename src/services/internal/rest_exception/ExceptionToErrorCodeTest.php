a81b6c5450539b26db78a2e22a900ea308feb016
Csizmarik Norbert <Csizmarik.Norbert@biztositas.hu>
2017-06-19 18:24:28 +0200

    test(services/rest_exception): unittest for translation from exception class to error code
    Signed-off-by: Csizmarik Norbert <Csizmarik.Norbert@biztositas.hu>

---
+++ src/services/internal/rest_exception/ExceptionToErrorCodeTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: csizmarik
+ * Date: 6/19/2017
+ * Time: 6:15 PM
+ */
+
+namespace services\internal\rest_exception;
+
+use PHPUnit\Framework\TestCase;
+use services\internal\user\FailedValidationException;
+
+class ExceptionToErrorCodeTest extends TestCase
+{
+	/**
+	 * @test
+	 */
+	public function basicExceptionShouldBeTranslatedTo500() {
+		$this->assertEquals(500,ExceptionToErrorCode::getCodeFor(\Exception::class));
+	}
+
+	/**
+	 * @test
+	 */
+	public function failedValidationExceptionShouldBeTranslatedTo400() {
+		$this->assertEquals(400,ExceptionToErrorCode::getCodeFor(FailedValidationException::class));
+	}
+}
